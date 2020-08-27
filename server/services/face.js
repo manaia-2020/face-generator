2440b63974cada3200b487e48e7051a039001476
Josiah Waikari <teariki.waikari@gmail.com>
2020-08-28 10:45:26 +1200

    Add function to get a generated face from random parts

--- server/services/face.js
+++ server/services/face.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 
 const randomFile = require('../helpers/random')
 
+const parts = ['ForeHeads', 'Eyes', 'Noses', 'Mouths', 'Chins']
+
 function getPart(part) {
   return new Promise((resolve, reject) => {
     fs.readdir(`../../public/images/${part}`, 'utf8', (err, files) => {
@@ -10,3 +12,16 @@ function getPart(part) {
     })
   })
 }
+
+async function getFace() {
+  const face = {}
+
+  for(let i = 0; i < parts.length; i++) {
+    const part = await getPart(parts[i])
+    face[parts[i]] = part
+  }
+
+  return face
+}
+
+module.exports = getFace
